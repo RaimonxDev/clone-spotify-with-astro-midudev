722ec4cf4372ed1aa6bf3259aa3f1b1840d7b2ec
Ramon <ramonmartinezcordero@gmail.com>
2023-12-29 09:07:09 -0300

    avances parte 2

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,15 @@
+<script lang="ts" >
+
+const currentTime = new Date()
+const currentHours = currentTime.getHours()
+let greeting = ''
+
+if (currentHours < 12) {
+    greeting = 'Good morning'
+} else if (currentHours < 18) {
+    greeting = 'Good afternoon'
+} else {
+    greeting = 'Good evening'
+}
+</script>
+<h1 class="text-4xl font-bold">{greeting}</h1>
